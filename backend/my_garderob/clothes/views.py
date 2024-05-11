5717674e543b4eee3c61e6cf36b75d17544dae2e
MsXeniya <xeniya.baranovskaya@mail.ru>
2024-05-11 20:16:36 +0300

    подсчет количества типов одежды и уровней

--- backend/my_garderob/clothes/views.py
+++ backend/my_garderob/clothes/views.py
@@ -1,9 +1,10 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import status
 from clothes.filters import LevelFilter
-from clothes.models import Seasons, Colors, TypeClothes, Clothes
+from clothes.models import Seasons, Colors, TypeClothes, Clothes, ClothesLevel
 from clothes.serializers import SeasonsSerializer, TypeClothesSerializer, ColorsSerializer, ClothesSerializer
 
 
@@ -40,3 +41,21 @@ class ClothesViewSet(ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(methods=["GET"], detail=False, url_path="count-of-levels")
+    def count_of_levels(self, request):
+        # Возвращает сколько вещей у пользователя для конкретного уровня одежды
+        levels_counts = {
+            level: request.user.clothes_set.filter(type_clothes__level=level).count()
+            for level in ClothesLevel.values
+        }
+        return Response(levels_counts, status=status.HTTP_200_OK)
+
+    @action(methods=["GET"], detail=False, url_path="count-of-clothes-types")
+    def count_of_clothes_types(self, request):
+        # Возвращает сколько вещей у пользователя для конкретного типа одежды
+        levels_counts = {
+            type_name: request.user.clothes_set.filter(type_clothes__name=type_name).count()
+            for type_name in TypeClothes.objects.all().values_list('name', flat=True)
+        }
+        return Response(levels_counts, status=status.HTTP_200_OK)
