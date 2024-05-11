362ce6a6a79a612b0952916d30ed02b18c88d221
MsXeniya <xeniya.baranovskaya@mail.ru>
2024-05-11 21:30:57 +0300

    фикс сохранения и полученяи картинок

--- backend/my_garderob/my_garderob/urls.py
+++ backend/my_garderob/my_garderob/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("users/", include("users.urls")),
     path("admin/", admin.site.urls),
     path("clothes/", include("clothes.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
