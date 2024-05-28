888467dda4640e70ffddef3f6fcc49b6e6588cce
renatsharaga <gutarov032@bk.ru>
2024-05-28 22:33:18 +0300

    .

--- backend/my_garderob/users/serializers.py
+++ backend/my_garderob/users/serializers.py
@@ -5,16 +5,16 @@ from rest_framework.authtoken.admin import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta:        #мета класс в котором прописанно в какой модели мы работаем и с какими полями
         model = User
         fields = ['email', 'username', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
+    def create(self, validated_data): #правила создания новых записей  при post def create при put def create
         user = User(
             email=validated_data['email'],
             username=validated_data['username']
         )
-        user.set_password(validated_data['password'])
-        user.save()
+        user.set_password(validated_data['password']) #хеширует пароль
+        user.save() #сохраняет юзера
         return user
